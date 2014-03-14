c6821f400accb081c53d0264ac0bba79895c55e6
Felix Thea <felixthea@gmail.com>
2014-03-14 17:36:29 -0400

    updated message sent from groupie

--- app.js
+++ app.js
@@ -87,11 +87,12 @@ bot.on('botMessage', function(bot, message) {
         bot.message(hedberg[Math.floor(Math.random() * hedberg.length)])
     } else if ((tokens.indexOf('calories') == 1)) {
     		tokens = _.without(tokens, 'groupie', 'g', 'calories');
+    		humanSearchTerm = tokens.join(' ');
     		searchTerm = escape(tokens.join('+'));
     		request('https://api.nutritionix.com/v1_1/search/' + searchTerm + '?results=0%3A1&cal_min=0&cal_max=5000&fields=item_name%2Cbrand_name%2Cnf_calories&appId=5acc25fb&appKey=056e335168bb29dd99fe206141ff84cc', function(err, resp, body){
     			json = eval("(" + body + ')');
     			cals = json.hits[0].fields.nf_calories
-    			bot.message("You ate " + cals + " calories.")
+    			bot.message("A " + humanSearchTerm + " is " + cals + " calories.")
     		});
     } else {
       bot.message("What?")
